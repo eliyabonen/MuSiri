253cd4e0bb836f83fd00fb686cfb9847f7b3d89a
EliyaBonen <eliyathebonen@walla.com>
2016-12-27 19:17:03 +0200

    added the the speak button and the google speech api option

--- MuSiri/app/src/main/java/com/musiri/musiri/MainActivity.java
+++ MuSiri/app/src/main/java/com/musiri/musiri/MainActivity.java
@@ -1,13 +1,58 @@
 package com.musiri.musiri;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity
+{
+    TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void onSpeakButtonClick(View view)
+    {
+        Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start speaking bitch!");
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+        startActivityForResult(speechRecognizerIntent, 1);
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if(requestCode == 1)
+        {
+            ArrayList<String> speech;
+
+            speech = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            String text = "";
+
+            for(int i = 0; i < speech.size(); i++)
+            {
+                text += speech.get(i);
+                text += " ";
+            }
+
+            textView.setText(text);
+        }
     }
 }
